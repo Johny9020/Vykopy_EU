9a20d9e34b3637d12ba091a992c51f7d5f8afa15
Johnny Jurak <johny9020@hotmail.com>
2023-05-27 18:20:17 +0200

    Fix

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <div class="dev">
+    <h1>Aktuálně probíhá vývoj této stránky. Děkujeme vám za pochopení.</h1>
+  </div>
+      <div class="headerMain">
+        <div class="nav_bar">
+          <h1 class="logo">Výkopy EU</h1>
+          <nav>
+            <ul class="nav_links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">O nás</a></li>
+              <li><a href="#">Fotogalerie</a></li>
+            </ul>
+          </nav>
+
+          <a class="cta" href="#"><button>Kontakt</button></a>
+
+          <div class="mobile_nav">
+            <i class="fa-solid fa-bars"></i>
+          </div>
+
+          <div class="dropdown_menu">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">O nás</a></li>
+              <li><a href="#">Fotogalerie</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <section id="welcome_text">
+          <h1>Vítejte na Výkopy EU,</h1>
+          <p>
+            kde poskytujeme špičkové a profesionální stavební práce, výkopové
+            práce a zemní práce.
+          </p>
+        </section>
+      </div>
+    </header>
+</html>
---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Vykopy EU</title>
+    <meta name="description" content="Vykopove prace" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- JQuery script -->
+    <script
+      src="https://code.jquery.com/jquery-3.6.4.min.js"
+      integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- Script.js -->
+    <script src="js/script.js"></script>
+
+    <!-- FontAwesome script -->
+    <script
+      src="https://kit.fontawesome.com/57cda63a00.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <!-- Header -->
+    
+    <?php include("header.php");?>
+
+    <main>
+      <div class="about_us_short">
+        <h1>O nás</h1>
+        <p id="about_us_short_p1">
+          Jsme specializovaná firma, která se věnuje stavebním pracím, zemním
+          pracím, výkopovým pracím a bouracím pracím.
+        </p>
+        <p id="about_us_short_p2">
+          Díky naší specializaci na stavební práce, zemní práce, výkopové práce
+          a bourací práce jsme schopni zajistit kompletní dodávku, včetně
+          srovnání podloží a položení zámkové dlažby. Kromě toho se také
+          specializujeme na výstavbu základových desek a provádíme kanalizační,
+          vodovodní a plynovodní přípojky. Naši odborníci jsou vysoce
+          kvalifikovaní a vždy dodržují přísné technologické postupy, aby
+          zajistili kvalitní výsledky práce. S námi můžete mít jistotu, že Vaše
+          stavební a zemní práce budou provedeny s precizností a v souladu s
+          Vašimi požadavky. Díky našemu týmu zkušených odborníků jsme schopni
+          zajistit kvalitní a rychlé provedení práce. Sledujeme nejnovější
+          trendy a technologie v oblasti stavebnictví a průběžně se vzděláváme,
+          abychom mohli našim zákazníkům nabídnout ty nejlepší služby a
+          materiály. Jsme schopni realizovat jak menší, tak i větší projekty.
+          Snažíme se vždy maximálně vyhovět potřebám našich zákazníků a při
+          každé práci dbáme na preciznost a spolehlivost. V naší práci klade
+          velký důraz na bezpečnost a dodržování právních předpisů.
+          Zabezpečujeme tedy, aby práce byly provedeny v souladu s platnými
+          předpisy a aby naše práce byly bezpečné pro naše pracovníky i pro
+          okolí. Provozujeme naše aktivity v Ústeckém kraji, Středočeském kraji
+          a v Praze.
+        </p>
+      </div>
+
+      <div class="our_products">
+        <div class="our_products_content">
+          <div class="our_products_content_left">
+            <!-- Left Content -->
+
+            <section class="divider">
+              <h1>Naše produkty</h1>
+              <p>
+                Krátký popis produktu zdůrazňující hlavní použití a vlastnosti
+                výrobku.
+              </p>
+            </section>
+
+            <section class="divider">
+              <h1>Motory a převodové motory</h1>
+              <p>
+                Krátký popis produktu zdůrazňující hlavní použití a vlastnosti
+                výrobku.
+              </p>
+            </section>
+
+            <section class="divider">
+              <h1>Indukční/kapacitní senzory</h1>
+              <p>
+                Krátký popis produktu zdůrazňující hlavní použití a vlastnosti
+                výrobku.
+              </p>
+            </section>
+
+            <section>
+              <h1>Inductive/Capacitive Sensors</h1>
+              <p>
+                Krátký popis produktu zdůrazňující hlavní použití a vlastnosti
+                výrobku.
+              </p>
+            </section>
+          </div>
+
+          <div class="our_products_content_right">
+            <!-- Right Content -->
+            <h1>
+              The only place where you’ll get the perfect solution for all your
+              industry needs.
+            </h1>
+
+            <a class="cta" href="#"><button>Kontakt</button></a>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
